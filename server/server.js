7fc419a843dba8f8e738ef141ed732ad2c18b20e
Gaurav Tomar <gauravtomar977@gmail.com>
2025-11-30 23:20:13 +0530

    .

--- server/server.js
+++ server/server.js
@@ -74,8 +74,10 @@ app.use("/api/messages" , messageRouter)
 
 await connectDb();
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
 server.listen(PORT,()=> console.log("server running on port : " + PORT));
+}
 
 //export server for vercel
 
